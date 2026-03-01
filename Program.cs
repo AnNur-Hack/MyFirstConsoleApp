467e0776b22a92100d8865e733e22f8aa3042af0
Ademola Adewuyi <ademolaadewuyi63@gmail.com>
2026-03-01 09:11:58 +0100

    my first rider project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿//Console.WriteLine("my name is ademola");
+//Console.WriteLine("I am tall");
+
+//Variable declarartion (int, double, string, bool)
+int age = 50;
+string name = "Ademola";
+double height = 32.5;
+bool isPresent = false;
+//Console.WriteLine(name + " " + height); //string concatenation
+//Console.WriteLine($"{name} {height}"); //string interpolation
+//Console.WriteLine($"my name is {name}, i am {age} years old. am i a christian? {isPresent}"); // string interpolation
+Console.WriteLine("my name is " + name + " and my age is " + age + "year old, my height is " + height + " ,am i a christian? " + isPresent);
