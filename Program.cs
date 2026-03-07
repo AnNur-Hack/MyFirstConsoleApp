2e9afe298cc687dd7f0ec85d2b741875293413d9
Ademola Adewuyi <ademolaadewuyi63@gmail.com>
2026-03-07 14:35:37 +0100

    submission of march7 classwork

--- Program.cs
+++ Program.cs
@@ -1,12 +1,61 @@
-﻿//Console.WriteLine("my name is ademola");
-//Console.WriteLine("I am tall");
+﻿//Task 1- ARITHEMETIC OPERATORS
 
-//Variable declarartion (int, double, string, bool)
-int age = 50;
-string name = "Ademola";
-double height = 32.5;
-bool isPresent = false;
-//Console.WriteLine(name + " " + height); //string concatenation
-//Console.WriteLine($"{name} {height}"); //string interpolation
-//Console.WriteLine($"my name is {name}, i am {age} years old. am i a christian? {isPresent}"); // string interpolation
-Console.WriteLine("my name is " + name + " and my age is " + age + "year old, my height is " + height + " ,am i a christian? " + isPresent);
+int a = 60;
+int b = 25;
+
+int printAddition = a + b;
+Console.WriteLine($"our answer= {printAddition}");
+
+int printSubtraction = a - b;
+Console.WriteLine($"our answer= {printSubtraction}");
+
+int printMultiplication = a * b;
+Console.WriteLine($"our answer= {printMultiplication}");
+
+double printDivision = (double)a / b;
+Console.WriteLine($"our answer= {printDivision}");
+
+int printModulus = a % b;
+Console.WriteLine($"our answer= {printModulus}");
+
+//Task 2- Integer vs Double Division
+
+int ab = 10;
+int bc = 4;
+
+int intDivision = ab / bc;
+Console.WriteLine($"our answer= {intDivision}");
+
+double doubleDivision = (double)ab / bc;
+Console.WriteLine($"our answer= {doubleDivision}");
+
+//Task 3- Large Numbers
+
+int population1 = 40000000;
+long population2 = 40000000000;
+
+long newPopulation = population1 + population2;
+Console.WriteLine($"newPopulation= {newPopulation}");
+
+//Task 4- Shop Calculator
+
+decimal price = 1500.75m;
+int quantity = 3;
+
+decimal totalCost = price * quantity;
+Console.WriteLine($"Total Price={totalCost}");
+
+//Task 5- Bonus Challenge
+
+int number1 = 20;
+int number2 = 30;
+int number3 = 40;
+
+int averageScore = (number1 + number2 + number3)/3;
+Console.WriteLine($"averageScore= {averageScore}");
+    
+    
+
+    
+
+
--- obj/Debug/net8.0/MyfirstconsoleAPP.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyfirstconsoleAPP.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyfirstconsoleAPP")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+467e0776b22a92100d8865e733e22f8aa3042af0")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyfirstconsoleAPP")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyfirstconsoleAPP")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
