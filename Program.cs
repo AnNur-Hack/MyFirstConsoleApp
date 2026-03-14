bd0ec45b81f43f4b432caf6bedfa8d7b8819c561
Ademola Adewuyi <ademolaadewuyi63@gmail.com>
2026-03-14 12:47:13 +0100

    updated classwork-march14 to github

--- Program.cs
+++ Program.cs
@@ -2,11 +2,60 @@
 //Console.WriteLine("I am tall");
 
 //Variable declarartion (int, double, string, bool)
-int age = 50;
+/*int age = 50;
 string name = "Ademola";
 double height = 32.5;
-bool isPresent = false;
+bool isPresent = false;*/
 //Console.WriteLine(name + " " + height); //string concatenation
 //Console.WriteLine($"{name} {height}"); //string interpolation
 //Console.WriteLine($"my name is {name}, i am {age} years old. am i a christian? {isPresent}"); // string interpolation
-Console.WriteLine("my name is " + name + " and my age is " + age + "year old, my height is " + height + " ,am i a christian? " + isPresent);
+//Console.WriteLine("my name is " + name + " and my age is " + age + "year old, my height is " + height + " ,am i a christian? " + isPresent);
+
+//LOOPS
+
+//for-loop, while-loop, DoWhile-loop
+
+/*int i = 1;
+
+while (i <= 5)
+{
+    Console.WriteLine(i);
+    i++;
+}*/
+
+//Do-while
+
+/*nt i = 1;
+
+do
+{
+    Console.WriteLine(i);
+    i++;
+} while (i <= 5);*/
+
+//for loop 
+
+//this is used when you know the number of times you to loop
+
+/*for (int i = 1; i <= 5; i++)
+{
+    Console.WriteLine(i);
+}
+
+int num = 99;
+
+do
+{
+    Console.WriteLine(num);
+    num++;
+}
+while (num <= 107);*/
+
+int multiplication = 6;
+
+for (int i = 1; i <= 12; i++)
+{
+    Console.WriteLine($"{multiplication} x {i} = {multiplication * i}");
+}
+    
+
