3b6474f67e7a2504723f3b2e278f3473aa89e2e1
Ademola Adewuyi <ademolaadewuyi63@gmail.com>
2026-03-17 16:17:13 +0100

    ATM MACHINE class project

--- Program.cs
+++ Program.cs
@@ -1,133 +1,82 @@
-﻿//Task 1- Student 
+﻿/*
+int[] temperatures = [25, 32, 28, 35, 30, 29, 40];
+var threshold = 30;
 
-/*int[] scores = { 45, 70, 55, 30, 90, 60 };
-for (int i = 0; i < scores.Length; i++)
+foreach (var t in temperatures)
 {
-    Console.WriteLine("scores: " + scores[i]); //concatenation
-}*/
-
-/*
-int[] scores = { 45, 70, 55, 30, 90, 60 };
-int highestScore = scores[0]; //We start by assuming the first score is both highest and lowest.
-int lowestScore = scores[0];
-
-
-for (int i = 1; i < scores.Length; i++) // we start from index 1 because index 0 is already assumed.
-{ if (scores[i] > highestScore)
+    if (t >= threshold) 
     {
-        highestScore = scores[i]; 
+        threshold = t;
+        Console.WriteLine($"temperature is {threshold}");
     }
-    if (scores[i] < lowestScore)
-    {
-        lowestScore = scores[i];
-    }
-}
-Console.WriteLine("Highest Score: " + highestScore); //concatenation
-Console.WriteLine("Lowest Score: " + lowestScore); //concatenation*/
-
-/*
-int[] scores = { 45, 70, 55, 30, 90, 60 };
-int sum = 0;
-
-for (int i = 0; i < scores.Length; i++) // Loop through each score and add to sum
-{
-    sum += scores[i];
-}
-decimal average = (decimal)sum / scores.Length; // we calculate the average, we used decimal because the output will be in decimals
-
-// Print the average
-Console.WriteLine($"Average score: {average}");*/
-
-/*
-int[] scores = { 45, 70, 55, 30, 90, 60 };
-
-int sum = 0; // sum of all scores
-
-// 1. Calculate total sum
-for (int i = 0; i < scores.Length; i++)
-{
-    sum += scores[i];
 }
 */
 
-// 2. Calculate average and round to 2 decimal places
-/*double average = ((double)sum / scores.Length);
-Console.WriteLine("Average score: " + average);
+//Atm Machine project
 
-// 3. Count number of students above average
-int countAboveAverage = 0;
-for (int i = 0; i < scores.Length; i++)
+int pin = 7071;
+int amount = 5000;
+string Acctname = "Lakan";
+int[] amountwithdraw = {2000,500,1000,5000};
+
+Console.WriteLine("Pls enter your pin");
+int pinentry = int.Parse(Console.ReadLine());
+if (pinentry == pin)
 {
-    if (scores[i] > average)
-    {
-        countAboveAverage++; // increment count if score > average
-    }
+ Console.WriteLine("Enter your account type\n(1) Press 1 for saving. \n (2) Press 2 for current.");
+ int accttypeEntry = int.Parse(Console.ReadLine());
+ if (accttypeEntry == 1)
+ {
+ Console.WriteLine("////////////////////////// \n(1) Press 1 to withdraw. \n (2) Press 2 to check balance.");
+ int bankoperation = int.Parse(Console.ReadLine());
+ if (bankoperation == 1)
+ {
+ Console.WriteLine("///////////////////////\n select amount to withraw\n(1) 2000. \n (2) 500 \n (3) 1000 \n (4) 5000 \n (5)Enter value you want to with");
+ int amountToWithdraw = int.Parse(Console.ReadLine());
+ // we move to switch case 
+ switch (amountToWithdraw)
+ {
+  case 1:
+   amount -= amountwithdraw[0];
+   Console.WriteLine($"//////Withdraw Alert///\n Hi {Acctname}, you have done atm transaction.\n TrasactionType = Debit.\n Amount-Withdrawn = {amountwithdraw[0]}. \n Balance = {amount}.\n Thank you for banking with us.");
+   break;
+  case 2:
+   amount -=  amountwithdraw[1];
+   Console.WriteLine($"////Withdraw Alert///\n Hi {Acctname}, you have done atm transaction.\n TrasactionType = Debit.\n Amount-Withdrawn = {amountwithdraw[1]}. \n Balance = {amount}.\n Thank you for banking with us.");
+   break;
+  case 3:
+   amount -= amountwithdraw[2];
+   Console.WriteLine($"////Withdraw Alert///\n Hi {Acctname}, you have done atm transaction.\n TransactionType = Debit.\n Amount-Withdrawn = {amountwithdraw[2]}. \n Balance = {amount}.\n Thank you for banking with us.");
+   break;
+  case 4:
+   amount -=  amountwithdraw[3];
+   Console.WriteLine($"////Withdraw Alert///\n Hi {Acctname}, you have done atm transaction.\n TrasnactionType = Debit.\n Amount-Withdrawn = {amountwithdraw[3]}. \n Balance = {amount}.\n Thank you for banking with us.");
+   break;
+  case 5:
+   amount -=  amountToWithdraw;
+   Console.WriteLine($"////Withdraw Alert///\n Hi {Acctname}, you have done atm transaction.\n TrasactionType = Debit.\n Amount-Withdrawn = {amountwithdraw[0]}. \n Balance = {amount}.\n Thank you for banking with us.");
+   break;
+  default:
+   Console.WriteLine("////////\nInvalid opration");
+   break;
+ }
+ }
+// Console.WriteLine($"/////AccountSummary///\n. welcome {Acctname}, Your Account Balance is #{amount}");
+ 
+ }
+ else
+ {
+  Console.WriteLine("//////////////////\nYour account type is invalid");
+ }
+ 
+}
+else
+{
+ Console.WriteLine("/////////Invalid pin, try again");
 }
 
-// 4. Print the result
-{
-    Console.WriteLine("Number of students above average: " + countAboveAverage);
-}
 
-for (int i = 0; i < scores.Length; i++) //iterate through all students.
-{
-    if (scores[i] >= 50) 
-    {
-        Console.WriteLine($"Student {i + 1}: Passed"); // interpolation
-    }
-    else
-    {
-        Console.WriteLine($"Student {i + 1}: failed"); //interpolation
-    }
-}*/
-
-//Task 2- Array filter and Reverse
-
-        // Array of temperatures
-        int[] temperatures = { 25, 32, 28, 35, 30, 29, 40 };
-        int threshold = 30; // Temperature threshold
-
-        // 1. Print all temperatures
-        Console.Write("All temperatures: ");
-        for (int i = 0; i < temperatures.Length; i++)
-        {
-            Console.Write(temperatures[i]);
-            if (i < temperatures.Length - 1) Console.Write(", ");
-        }
-        Console.WriteLine(); // Move to next line
-
-        // 2. Print only temperatures above threshold
-        Console.Write($"Temperatures above {threshold}: ");
-        for (int i = 0; i < temperatures.Length; i++)
-        {
-            if (temperatures[i] > threshold)
-            {
-                Console.Write(temperatures[i] + " ");
-            }
-        }
-        Console.WriteLine(); // Move to next line
-
-        // 3. Reverse the array and print
-        Console.Write("Reversed temperatures: ");
-        for (int i = temperatures.Length - 1; i >= 0; i--)
-        {
-            Console.Write(temperatures[i]);
-            if (i > 0) Console.Write(", ");
-        }
-        Console.WriteLine(); // Move to next line
-
-        // 4. Calculate and print average temperature
-        int sum = 0;
-        for (int i = 0; i < temperatures.Length; i++)
-        {
-            sum += temperatures[i];
-        }
-
-        {
-            double average = (double)sum / temperatures.Length;
-            Console.WriteLine("Average temperature: " + average);
-        }
-    
+ 
 
 
 
--- obj/Debug/net8.0/MyfirstconsoleAPP.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyfirstconsoleAPP.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyfirstconsoleAPP")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd0ec45b81f43f4b432caf6bedfa8d7b8819c561")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c71a247ad9c501cad13b64a525799ab5eb169440")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyfirstconsoleAPP")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyfirstconsoleAPP")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
